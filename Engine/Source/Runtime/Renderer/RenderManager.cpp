2b8a1c8961f7c2b19434118893a090f2220ca644
MrDrElliot <133075253+MrDrElliot@users.noreply.github.com>
2026-05-10 14:34:49 -0500

    turn off validation by default

--- Engine/Source/Runtime/Renderer/RenderManager.cpp
+++ Engine/Source/Runtime/Renderer/RenderManager.cpp
@@ -61,7 +61,7 @@ namespace Lumina
         #if LUMINA_SHIPPING
         GRenderContext->Initialize(FRenderContextDesc{false, false});
         #else
-        GRenderContext->Initialize(FRenderContextDesc{true, true});
+        GRenderContext->Initialize(FRenderContextDesc{false, true});
         #endif
         
         #if WITH_EDITOR
